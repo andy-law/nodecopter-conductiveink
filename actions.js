6e0d875fc4627fcd967e39497d13dc7455a3f0c9
Shane Hudson <shane@shanehudson.net>
2013-03-16 12:33:54 +0000

    More actions

--- actions.js
+++ actions.js
@@ -22,4 +22,28 @@ Actions.prototype.toggleFlying = function()  {
 	else this.takeoff();
 }
 
+Actions.prototype.up = function(speed)  {
+	client.up(speed || 0.5);
+}
+
+Actions.prototype.down = function(speed)  {
+	client.down(speed || 0.5);
+}
+
+Actions.prototype.left = function(speed)  {
+	client.left(speed || 0.5);
+}
+
+Actions.prototype.right = function(speed)  {
+	client.right(speed || 0.5);
+}
+
+Actions.prototype.front = function(speed)  {
+	client.front(speed || 0.5);
+}
+
+Actions.prototype.back = function(speed)  {
+	client.back(speed || 0.5);
+}
+
 module.exports = Actions;
